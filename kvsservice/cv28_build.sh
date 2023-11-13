5d02d59a5458416d2819b6e1e220a0c697b9e8ce
jameslu <james.lc.lu@foxconn.com>
2023-11-13 10:11:57 +0800

    could build db in the version

---
+++ kvsservice/cv28_build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+
+AARCH64_TOOLCHAIN_PATH=/usr/local/linaro-aarch64-2020.09-gcc10.2-linux5.4/bin
+CROSS_COMPILE=${AARCH64_TOOLCHAIN_PATH}/aarch64-linux-gnu
+
+export CC="${CROSS_COMPILE}-gcc -march=armv8-a -mcpu=cortex-a53+crypto"
+export CXX="${CROSS_COMPILE}-g++"
+export AR="${CROSS_COMPILE}-ar"
+export LD="${CROSS_COMPILE}-ld"
+
+export AS="${CROSS_COMPILE}-as"
+export STRIP="${CROSS_COMPILE}-strip"
+export RANLIB="${CROSS_COMPILE}-ranlib"
+export OBJCOPY="${CROSS_COMPILE}-objcopy"
+export OBJDUMP="${CROSS_COMPILE}-objdump"
+export READELF="${CROSS_COMPILE}-readelf"
+export NM="${CROSS_COMPILE}-nm"
+
+rm -r doorbell-build
+mkdir doorbell-build
+cd doorbell-build
+cmake ./../kvsservice
+make -j4
+
+echo "cv28 env setup success."
